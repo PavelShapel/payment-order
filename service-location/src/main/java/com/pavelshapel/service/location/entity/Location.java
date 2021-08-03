bad995599fecfaeb4280c592bd1cae4e0b481b3a
Pavel, Shapel <pavel.shapel@ukg.com>
2021-08-03 19:25:10 +0300

    refactor > payment-order > Location.java

--- service-location/src/main/java/com/pavelshapel/service/location/entity/Location.java
+++ service-location/src/main/java/com/pavelshapel/service/location/entity/Location.java
@@ -1,5 +1,7 @@
 package com.pavelshapel.service.location.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.pavelshapel.jpa.spring.boot.starter.entity.AbstractAuditableVersionEntity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -17,15 +19,29 @@ import javax.validation.constraints.Size;
 public class Location extends AbstractAuditableVersionEntity {
     @ManyToOne(fetch = FetchType.LAZY, targetEntity = Location.class)
     @JoinColumn(name = "parent_id")
+    @JsonIgnore
     private Location parent;
 
     @ManyToOne(fetch = FetchType.LAZY, targetEntity = LocationType.class)
     @JoinColumn(name = "location_type_id")
+    @JsonIgnore
     private LocationType locationType;
 
     @Column(nullable = false)
     @NotBlank(message = MANDATORY)
     @Size(max = Byte.MAX_VALUE)
     private String name = MANDATORY;
+
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    @Transient
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Long locationTypeId;
+
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    @Transient
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Long parentId;
 }
 
