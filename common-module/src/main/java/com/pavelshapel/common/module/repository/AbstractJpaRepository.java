2c166be69a766f381df945c094e949b329232460
Pavel, Shapel <pavel.shapel@ukg.com>
2021-08-04 14:24:21 +0300

    remove > common-module > unnecessary files

--- common-module/src/main/java/com/pavelshapel/common/module/repository/AbstractJpaRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.pavelshapel.common.module.repository;
-//
-//import com.pavelshapel.common.module.entity.AbstractEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface AbstractJpaRepository<T extends AbstractEntity> extends JpaRepository<T, Long> {
-//}
