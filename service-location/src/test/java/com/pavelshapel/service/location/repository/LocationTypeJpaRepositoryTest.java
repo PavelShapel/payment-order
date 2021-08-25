2a02c39c6b287cd425f7f424694326219e14bd29
Pavel, Shapel <pavel.shapel@ukg.com>
2021-08-25 09:31:49 +0300

    refactor > payment-order > update test for repository package

--- service-location/src/test/java/com/pavelshapel/service/location/repository/LocationTypeJpaRepositoryTest.java
+++ service-location/src/test/java/com/pavelshapel/service/location/repository/LocationTypeJpaRepositoryTest.java
@@ -1,23 +1,15 @@
 package com.pavelshapel.service.location.repository;
 
-import com.pavelshapel.jpa.spring.boot.starter.JpaAuditingConfiguration;
 import com.pavelshapel.jpa.spring.boot.starter.repository.search.SearchCriteria;
 import com.pavelshapel.jpa.spring.boot.starter.repository.search.SearchOperation;
-import com.pavelshapel.service.location.AbstractTest;
 import com.pavelshapel.service.location.ServiceLocationApplication;
 import com.pavelshapel.service.location.entity.LocationType;
 import com.pavelshapel.service.location.provider.OneStringProvider;
 import com.pavelshapel.service.location.provider.TwoStringProvider;
 import com.pavelshapel.service.location.repository.search.LocationTypeSearchSpecification;
-import com.pavelshapel.test.spring.boot.starter.container.postgres.PostgreSQLExtension;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ContextConfiguration;
 
 import javax.validation.ConstraintViolationException;
@@ -25,31 +17,21 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 
-@DataJpaTest
 @ContextConfiguration(classes = {
         ServiceLocationApplication.class,
-        JpaAuditingConfiguration.class
+        LocationTypeSearchSpecification.class
 })
-@AutoConfigureTestDatabase(replace = NONE)
-@ExtendWith(PostgreSQLExtension.class)
-class LocationTypeJpaRepositoryTest extends AbstractTest {
-    @Autowired
-    private TestEntityManager testEntityManager;
-    @Autowired
-    private LocationTypeJpaRepository locationTypeJpaRepository;
-
+class LocationTypeJpaRepositoryTest extends AbstractLocationTypeJpaRepositoryTest {
     @ParameterizedTest
     @ArgumentsSource(OneStringProvider.class)
     void findAllByNameContaining_WithValidParam_ShouldSaveAndReturnListWithEntity(String name) {
         LocationType locationType = getLocationType(name);
-        LocationType savedLocationType = testEntityManager.persistFlushFind(locationType);
-        SearchCriteria searchCriteriaName = getSearchCriteriaName(name, SearchOperation.CONTAINS);
-        LocationTypeSearchSpecification locationTypeSearchSpecification = new LocationTypeSearchSpecification();
-        locationTypeSearchSpecification.setSearchCriteria(searchCriteriaName);
+        LocationType savedLocationType = saveAndRetrieve(locationType);
+        SearchCriteria searchCriteriaName = getSearchCriteriaName(name.toLowerCase(), SearchOperation.CONTAINS);
+        getSearchSpecification().setSearchCriteria(searchCriteriaName);
 
-        List<LocationType> retrievedLocationType = locationTypeJpaRepository.findAll(locationTypeSearchSpecification);
+        List<LocationType> retrievedLocationType = getJpaRepository().findAll(getSearchSpecification());
 
         assertThat(retrievedLocationType)
                 .isNotNull()
@@ -61,22 +43,53 @@ class LocationTypeJpaRepositoryTest extends AbstractTest {
     @ArgumentsSource(TwoStringProvider.class)
     void findAllByNameContaining_WithInvalidParam_ShouldSaveAndReturnEmptyList(String name, String searchName) {
         LocationType locationType = getLocationType(name);
-        testEntityManager.persistFlushFind(locationType);
+        saveAndRetrieve(locationType);
         SearchCriteria searchCriteriaName = getSearchCriteriaName(searchName, SearchOperation.CONTAINS);
-        LocationTypeSearchSpecification locationTypeSearchSpecification = new LocationTypeSearchSpecification();
-        locationTypeSearchSpecification.setSearchCriteria(searchCriteriaName);
+        getSearchSpecification().setSearchCriteria(searchCriteriaName);
 
-        List<LocationType> retrievedLocationType = locationTypeJpaRepository.findAll(locationTypeSearchSpecification);
+        List<LocationType> retrievedLocationType = getJpaRepository().findAll(getSearchSpecification());
 
         assertThat(retrievedLocationType)
                 .isNotNull()
                 .isEmpty();
     }
 
+    @ParameterizedTest
+    @ArgumentsSource(OneStringProvider.class)
+    void findAllByNameStartsWith_WithValidParam_ShouldSaveAndReturnListWithEntity(String name) {
+        LocationType locationType = getLocationType(name);
+        LocationType savedLocationType = saveAndRetrieve(locationType);
+        SearchCriteria searchCriteriaName = getSearchCriteriaName(name.substring(0,1), SearchOperation.STARTS_WITH);
+        getSearchSpecification().setSearchCriteria(searchCriteriaName);
+
+        List<LocationType> retrievedLocationType = getJpaRepository().findAll(getSearchSpecification());
+
+        assertThat(retrievedLocationType)
+                .isNotNull()
+                .isNotEmpty()
+                .contains(savedLocationType);
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(OneStringProvider.class)
+    void findAllByNameEqual_WithValidParam_ShouldSaveAndReturnListWithEntity(String name) {
+        LocationType locationType = getLocationType(name);
+        LocationType savedLocationType = saveAndRetrieve(locationType);
+        SearchCriteria searchCriteriaName = getSearchCriteriaName(name, SearchOperation.EQUAL);
+        getSearchSpecification().setSearchCriteria(searchCriteriaName);
+
+        List<LocationType> retrievedLocationType = getJpaRepository().findAll(getSearchSpecification());
+
+        assertThat(retrievedLocationType)
+                .isNotNull()
+                .isNotEmpty()
+                .contains(savedLocationType);
+    }
+
     @Test
-    void persistFlushFind_WithNullParam_ShouldThrowException() {
+    void saveAndRetrieve_WithNullParam_ShouldThrowException() {
         LocationType locationType = getLocationType(null);
-        assertThatThrownBy(() -> testEntityManager.persistFlushFind(locationType))
+        assertThatThrownBy(() -> saveAndRetrieve(locationType))
                 .isInstanceOf(ConstraintViolationException.class);
     }
 }
