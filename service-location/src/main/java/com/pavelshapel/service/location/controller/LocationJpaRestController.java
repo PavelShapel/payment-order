fbb09e7c7e8faf13caf99debfa21dc17dbd958cc
Pavel, Shapel <pavel.shapel@ukg.com>
2021-10-01 11:34:27 +0300

    service-location: refactor LocationJpaRestController.java

--- service-location/src/main/java/com/pavelshapel/service/location/controller/LocationJpaRestController.java
+++ service-location/src/main/java/com/pavelshapel/service/location/controller/LocationJpaRestController.java
@@ -3,13 +3,18 @@ package com.pavelshapel.service.location.controller;
 import com.pavelshapel.jpa.spring.boot.starter.repository.search.SearchSpecification;
 import com.pavelshapel.jpa.spring.boot.starter.service.jpa.JpaService;
 import com.pavelshapel.service.location.entity.Location;
+import com.pavelshapel.service.location.entity.LocationType;
 import com.pavelshapel.web.spring.boot.starter.controller.AbstractJpaRestController;
 import com.pavelshapel.web.spring.boot.starter.controller.converter.FromDtoConverter;
 import com.pavelshapel.web.spring.boot.starter.controller.converter.ToDtoConverter;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static com.pavelshapel.service.location.controller.LocationJpaRestController.HOME_PATH;
 
@@ -20,10 +25,28 @@ public class LocationJpaRestController extends AbstractJpaRestController<Locatio
     public static final String HOME_PATH = "/locations" + StringUtils.EMPTY;
 
     @Autowired
+    private JpaService<LocationType> locationTypeJpaService;
+
+    @Autowired
     public LocationJpaRestController(JpaService<Location> locationJpaService,
                                      SearchSpecification<Location> locationSearchSpecification,
                                      ToDtoConverter<Location> locationToDtoConverter,
                                      FromDtoConverter<Location> locationFromDtoConverter) {
         super(locationJpaService, locationSearchSpecification, locationFromDtoConverter, locationToDtoConverter);
+    }
+
+    @GetMapping({"/resolve" + ID_PATH})
+    public ResponseEntity<String> getResolvedLocation(@RequestParam(required = false, defaultValue = "false") Boolean reverse, @PathVariable Long id) {
+        String dto = Optional.ofNullable(getParentage(reverse, id).getBody()).orElse(Collections.emptyList())
+                .stream().map(this::resolveLocation)
+                .collect(Collectors.joining(","));
+        return ResponseEntity.ok(dto);
+    }
+
+    private String resolveLocation(Location location) {
+        Long locationTypeId = location.getLocationTypeId();
+        LocationType locationType = locationTypeJpaService.findById(locationTypeId);
+        String locationTypeName = locationType.getName();
+        return String.format("%s %s", locationTypeName, location.getName());
     }
 }
