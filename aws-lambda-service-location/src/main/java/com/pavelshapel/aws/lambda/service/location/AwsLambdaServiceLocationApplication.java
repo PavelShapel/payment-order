2b14e2bc354f8879fe3dac70620aeb4984a6a7d2
Pavel, Shapel <pavel.shapel@ukg.com>
2022-04-02 15:03:31 +0300

    aws-lambda-service-location: refactor

--- aws-lambda-service-location/src/main/java/com/pavelshapel/aws/lambda/service/location/AwsLambdaServiceLocationApplication.java
+++ aws-lambda-service-location/src/main/java/com/pavelshapel/aws/lambda/service/location/AwsLambdaServiceLocationApplication.java
@@ -1,12 +1,28 @@
 package com.pavelshapel.aws.lambda.service.location;
 
+import com.pavelshapel.aws.lambda.service.location.web.LocationDaoRestController;
+import com.pavelshapel.common.module.dto.service.location.aws.LocationDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+import java.util.List;
+import java.util.function.Supplier;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class AwsLambdaServiceLocationApplication {
+    @Autowired
+    private LocationDaoRestController locationDaoRestController;
+
     public static void main(String[] args) {
         SpringApplication.run(AwsLambdaServiceLocationApplication.class, args);
+    }
+
+    @Bean
+    public Supplier<ResponseEntity<List<LocationDto>>> findAll() {
+        return () -> locationDaoRestController.findAll();
     }
 }
