610c75680b1bad88fb2a0e9524ef03f8024dc789
Pavel, Shapel <pavel.shapel@ukg.com>
2022-04-22 16:28:11 +0300

    aws-lambda-service-file-placeholder: add new module

---
+++ aws-lambda-service-file-placeholder/src/main/java/com/pavelshapel/aws/lambda/service/file/placeholder/App.java
@@ -0,0 +1,42 @@
+package com.pavelshapel.aws.lambda.service.file.placeholder;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Handler for requests to Lambda function.
+ */
+public class App implements RequestHandler<SQSEvent, Void> {
+
+    public Void handleRequest(final SQSEvent input, final Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("X-Custom-Header", "application/json");
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                .withHeaders(headers);
+        try {
+            final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
+            String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
+
+            return response
+                    .withStatusCode(200)
+                    .withBody(output);
+        } catch (IOException e) {
+            return response
+                    .withBody("{}")
+                    .withStatusCode(500);
+        }
+    }
+}
