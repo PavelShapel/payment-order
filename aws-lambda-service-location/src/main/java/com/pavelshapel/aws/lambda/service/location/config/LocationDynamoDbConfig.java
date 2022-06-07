56f7b911084312bc0d9995aafd7f5b9462906f22
Pavel, Shapel <pavel.shapel@ukg.com>
2022-06-07 13:51:24 +0300

    aws-lambda-service-location: refactor

--- aws-lambda-service-location/src/main/java/com/pavelshapel/aws/lambda/service/location/config/LocationDynamoDbConfig.java
+++ aws-lambda-service-location/src/main/java/com/pavelshapel/aws/lambda/service/location/config/LocationDynamoDbConfig.java
@@ -1,8 +1,10 @@
 package com.pavelshapel.aws.lambda.service.location.config;
 
 import com.pavelshapel.aws.lambda.service.location.repository.LocationDaoRepository;
-import com.pavelshapel.aws.spring.boot.starter.config.AbstractDynamoDbAwsConfig;
+import com.pavelshapel.aws.spring.boot.starter.config.AbstractDynamoDbAwsConfiguration;
+import com.pavelshapel.aws.spring.boot.starter.properties.AwsProperties;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -11,5 +13,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @Configuration
 @EnableDynamoDBRepositories(basePackageClasses = {LocationDaoRepository.class})
 @EnableJpaRepositories(excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {LocationDaoRepository.class})})
-public class LocationDynamoDbConfig extends AbstractDynamoDbAwsConfig {
+public class LocationDynamoDbConfig extends AbstractDynamoDbAwsConfiguration {
+    public LocationDynamoDbConfig(@Autowired AwsProperties awsProperties) {
+        super(awsProperties);
+    }
 }
