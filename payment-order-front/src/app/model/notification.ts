3df074c903e39860c32f18e9e4cd1206b821c8a7
Pavel, Shapel <pavel.shapel@ukg.com>
2023-04-08 12:10:06 +0200

    payment-order-front: add model notification.ts

---
+++ payment-order-front/src/app/model/notification.ts
@@ -0,0 +1,18 @@
+export const NOTIFICATION = {
+    success: {
+        value: 'SUCCESS',
+        class: 'text-color-success'
+    },
+    error: {
+        value: 'ERROR',
+        class: 'text-color-error'
+    },
+    info: {
+        value: 'INFO',
+        class: 'text-color-info'
+    },
+    default: {
+        value: '',
+        class: 'text-color-default'
+    }
+}
