1c4d7cb927dcf00bda1c6004b69cd9232f7b5e3f
Pavel, Shapel <pavel.shapel@ukg.com>
2023-04-10 12:25:21 +0200

    payment-order-front: add new rote

---
+++ payment-order-front/src/app/component/body/body.component.spec.ts
@@ -0,0 +1,28 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BodyComponent } from './body.component';
+
+describe('BodyComponent', () => {
+  let component: BodyComponent;
+  let fixture: ComponentFixture<BodyComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        BodyComponent
+      ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+      ]
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(BodyComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
